1dca4d3daff46ecd8c7e7b71b18599fbdcdef6f6
Jack Wright <jackw221001@gmail.com>
2024-03-02 16:55:17 +0000

    feat: add entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello world!");
+#!/usr/bin/env node
+
+import Parser from "./Parser";
+import OutputBuilder from "./OutputBuilder";
+
+const args = process.argv.splice(2);
+
+if (!args.length) {
+  console.log("No args provided");
+  process.exit(0);
+}
+
+try {
+  const argParser = new Parser(args);
+
+  const flags = argParser.getFlags();
+  const files = argParser.getFiles();
+
+  if (!files.length) {
+    console.log("No files provided");
+  }
+
+  files.forEach((filePath) => {
+    const outputBuilder = new OutputBuilder(flags, filePath);
+    const output = outputBuilder.getOutput();
+
+    console.log(output);
+  });
+} catch (err: unknown) {
+  console.log(err);
+}
